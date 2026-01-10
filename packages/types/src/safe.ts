ed599acc76ca35dcb77f460b1e456ef2f9f1e336
Mihail Kirov <me@mikirov.xyz>
2026-01-10 17:52:38 +0200

    rm:radash

--- packages/types/src/safe.ts
+++ packages/types/src/safe.ts
@@ -1,5 +1,3 @@
-import * as _ from 'radash'
-
 /**
  * Set of functions to wrap around promises to make them safe
  * Also works to wrap around try catch statements
@@ -26,57 +24,4 @@ export function safeErrorStr<T extends string>(err: T): SafeError<T> {
 
 export function safeError<E extends Error>(err: E): SafeError<E> {
   return [err, undefined]
-}
-
-export async function safeTry<T>(promise: Promise<T>): SafePromise<T> {
-  return _.try(() => promise)()
-}
-
-export const safeTimeoutPromiseAll = async <T>(
-  promises: Promise<T>[],
-  ms: number,
-): SafePromise<T[]> => {
-  const controller = new AbortController()
-  const signal = controller.signal
-
-  setTimeout(() => {
-    controller.abort(new Error(`Timeout after ${ms}ms`))
-  }, ms)
-
-  const [error, result] = await _.try(() => Promise.all(promises))()
-
-  if (signal.aborted) {
-    return safeError(
-      signal.reason instanceof Error
-        ? signal.reason
-        : new Error(String(signal.reason)),
-    )
-  }
-
-  if (error) return safeError(error)
-  return safeResult(result)
-}
-
-export const safeTimeoutPromise = async <T>(
-  promise: Promise<T>,
-  ms: number,
-): SafePromise<T> => {
-  const controller = new AbortController()
-  const signal = controller.signal
-
-  setTimeout(() => {
-    controller.abort(new Error(`Timeout after ${ms}ms`))
-  }, ms)
-
-  const [error, result] = await _.try(() => promise)()
-  if (signal.aborted) {
-    return safeError(
-      signal.reason instanceof Error
-        ? signal.reason
-        : new Error(String(signal.reason)),
-    )
-  }
-
-  if (error) return safeError(error)
-  return safeResult(result)
 }
